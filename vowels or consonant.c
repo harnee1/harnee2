72c3b4652233af1ee11465c27d6bcb220edc153b
harnee1 <43338341+harnee1@users.noreply.github.com>
2019-01-03 14:46:48 +0530

    Create vowels or consonant.c

---
+++ vowels or consonant.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+ char ch;
+ printf("Enter an alphabet:");
+ scanf("%c",&c);
+ if(c>='A'&&c<='Z')||(c>='a'&&c<='z')
+ {
+ switch(ch)
+ {
+ case 'A':
+ case 'E':
+ case 'I':
+ case 'O':
+ case 'U':
+ printf("%c is constant",&ch);
+ break();
+ else
+ {
+ printf("%c is consonant",&ch);
+ }
+ default("invalid");
+ }
+ return 0;
+ getch();
+ }
