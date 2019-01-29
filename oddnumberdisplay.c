f042193ca6112d42b8b0767792abeeefd243d4ec
harnee1 <43338341+harnee1@users.noreply.github.com>
2019-01-29 12:27:21 +0530

    Create oddnumberdisplay.c

---
+++ oddnumberdisplay.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,i;
+ printf("enter the two limits");
+ scanf("%d%d",&a,&b);
+ for(i=a;i<b;i++)
+ {
+  if(i%2==1)
+  {
+   printf("%d",i);
+}
+}
+}
