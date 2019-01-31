79f13cd85da92ee1fcc92e1399075e270c2ff91d
harnee1 <43338341+harnee1@users.noreply.github.com>
2019-01-31 14:49:16 +0530

    Create stringisnumeric.java

---
+++ stringisnumeric.java
@@ -0,0 +1,11 @@
+public static boolean isNumeric(String str)  
+{  
+ try  {  
+double d = Double.parseDouble(str);  
+}  
+catch(NumberFormatException nfe)  
+{  
+ return false;  
+ }  
+ return true;  
+}
