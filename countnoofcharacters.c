8751f25a4845897987f432edc21793b770a0bfd3
harnee1 <43338341+harnee1@users.noreply.github.com>
2019-02-21 14:18:44 +0530

    Create countnoofcharacters.c

---
+++ countnoofcharacters.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s[50];
+gets(s);
+int n=strlen(s);
+int i,count=0;
+for(i=0;i!=n;i++)
+{
+if(s[i]!=' ')
+{
+count++;
+}
+}
+printf("%d",count);
+}
